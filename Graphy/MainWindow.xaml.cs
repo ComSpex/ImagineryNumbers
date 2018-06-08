a537de1e3aa2f063fca8368d4ce9095eca5d1d33
Yosuke Maeda <yosuke_maeda@hotmail.com>
2018-06-08 16:44:58 +0900

    1.0

---
+++ Graphy/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Graphy {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow:Window {
+		Point origin;
+		double wide, high;
+		bool initialState = true;
+		int index = 0;
+		Point[] leftuppers;
+		private void box_MouseDown(object sender,MouseButtonEventArgs e) {
+			if(leftuppers==null) {
+				MessageBox.Show("First, make the window resized!!",this.Title,button:MessageBoxButton.OK,icon:MessageBoxImage.Information);
+				return;
+			}
+			Point leftupper = leftuppers[index];
+			Canvas.SetTop(box,leftupper.Y);
+			Canvas.SetLeft(box,leftupper.X);
+			if(++index>=leftuppers.Length) {
+				index=0;
+			}
+		}
+		public MainWindow() {
+			InitializeComponent();
+			this.SizeChanged+=(sender,e) => {
+				double a_width = e.NewSize.Width;
+				double aheight = e.NewSize.Height;
+				Debug.Assert(a_width==ActualWidth);
+				Debug.Assert(aheight==ActualHeight);
+				wide = a_width-2*SystemParameters.ResizeFrameVerticalBorderWidth;
+				high = aheight-2*SystemParameters.ResizeFrameHorizontalBorderHeight-SystemParameters.CaptionHeight;
+				origin = new Point(wide/2-6,high/2-6);
+				Lx.X2=wide;
+				Lx.Y1=Lx.Y2=origin.Y;
+				Ly.Y2=high;
+				Ly.X1=Ly.X2=origin.X;
+				if(!initialState) {
+					box.Width=wide/2;
+					box.Height=high/2;
+					leftuppers =new Point[]{new Point(0,0),new Point(origin.X,0),new Point(0,origin.Y),new Point(origin.X,origin.Y) };
+				}
+				initialState=false;
+			};
+			for(int i = 0;i<MainGrid.Children.Count;++i) {
+				UIElement elem = MainGrid.Children[i];
+				Debug.WriteLine(elem.ToString());
+				if(elem is System.Windows.Controls.TextBlock) {
+					TextBlock co = elem as TextBlock;
+					if(co.Name.Contains("Tx")) {
+						//MainGrid.Children.Remove(elem);
+						co.Visibility=Visibility.Hidden;
+					}
+				}
+			}
+		}
+	}
+}
--- ImagineryNumbers/Program.cs
+++ ImagineryNumbers/Program.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace ImagineryNumbers {
 	class Program {
 		static void Main(string[] args) {
+			Console.WriteLine("{0}",Math.Sqrt(-9.0));
+			Complex comp = Complex.Sqrt(-9.0);
+			Console.WriteLine("{0},{1}",comp,comp.Magnitude);
+			string magnitude = "Magnitude is the distance of the point from the origin(that is, 0,0, or the point at which the x-axis and the y-axis intersect). ";
+			string phase = "Phase is the angle between the real axis and the line drawn from the origin to the point.";
+			for(double i = -5;i<=5;++i) {
+				for(double r = -5;r<=5;++r) {
+					Complex cn = new Complex(r,i);
+					Console.Write("({0},{1:0.00},{2:0.00})",cn,cn.Magnitude,cn.Phase);
+				}
+				Console.WriteLine();
+			}
 		}
 	}
 }
